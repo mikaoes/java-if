bd5c464e0138e1b942cc5d66f79493ed92a246df
Mika <mika.oesterreich@icloud.com>
2024-06-07 08:43:18 +0200

    fix 21-binconv

--- 21-binconv/Konverter.java
+++ 21-binconv/Konverter.java
@@ -1,30 +1,33 @@
-
 /**
  * Beschreiben Sie hier die Klasse Konverter.
- * 
- * @author (Ihr Name) 
+ *
+ * @author (Ihr Name)
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class Konverter
-{
-    public static String conv (int zahl) {
+public class Konverter {
+
+    public static String conv(int zahl) {
         String ergebnis = "";
-        while (zahl>=1) {
-            ergebnis = ergebnis + zahl%2;
-            zahl = zahl/2;
+        while (zahl > 0) {
+            ergebnis = (zahl % 2) + ergebnis;
+            zahl = zahl / 2;
         }
         return ergebnis;
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public static void main(String args[])
-    {
-        // tragen Sie hier den Code ein
-        System.out.println(conv(100));
+    public static void main(String args[]) {
+        for (int i : new int[] { 0, 10, 25, 100, 256, 738 }) {
+            System.out.println(i + " -> " + conv(i));
+        }
     }
+
+    /*
+     * Korrekte Ergebnisse:
+     * 0 -> 0
+     * 10 -> 1010
+     * 25 -> 11001
+     * 100 -> 1100100
+     * 256 -> 100000000
+     * 738 -> 1011100010
+     */
 }
